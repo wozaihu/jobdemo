2252aeb9a925ba0a42544b82bf180fa70e9012f5
lyx <lyx.92@qq.com>
2023-01-29 17:52:52 +0800

    设置BottomSheetDialogFragment高度为屏幕百分比

--- app/src/main/java/com/example/jobdemo/kotlin_code/dialog/BottomDialog.kt
+++ app/src/main/java/com/example/jobdemo/kotlin_code/dialog/BottomDialog.kt
@@ -1,7 +1,7 @@
 package com.example.jobdemo.kotlin_code.dialog
 
+import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
@@ -11,6 +11,7 @@ import com.example.jobdemo.databinding.LayoutBottomDialogBinding
 import com.example.jobdemo.kotlin_code.adapter.BottomDialogAdapter
 import com.example.jobdemo.util.ToastUtils
 import com.example.jobdemo.util.Utils
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.divider.MaterialDividerItemDecoration
 
@@ -22,16 +23,38 @@ import com.google.android.material.divider.MaterialDividerItemDecoration
 
  */
 class BottomDialog : BottomSheetDialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding = LayoutBottomDialogBinding.inflate(inflater, container, false)
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val binding = LayoutBottomDialogBinding.inflate(inflater, container, false)
+//        val defaultSentenceArray: Array<String> = if (Utils.getUserType(context)) {
+//            resources.getStringArray(R.array.companyOftenSentence)
+//        } else {
+//            resources.getStringArray(R.array.normalOftenSentence)
+//        }
+//        val list: MutableList<String> = ArrayList()
+//        list.addAll(defaultSentenceArray)
+//        binding.rvSentenceList.layoutManager = LinearLayoutManager(context)
+//        context?.let {
+//            val decoration =
+//                MaterialDividerItemDecoration(it, MaterialDividerItemDecoration.VERTICAL)
+//            decoration.dividerColor = ContextCompat.getColor(it, R.color.lightGray)
+//            binding.rvSentenceList.addItemDecoration(decoration)
+//        }
+//        binding.rvSentenceList.adapter = BottomDialogAdapter(list)
+//        binding.tvAdd.setOnClickListener { ToastUtils.shortToast(context, "点击了添加") }
+//        binding.tvRevamp.setOnClickListener { ToastUtils.shortToast(context, "点击了修改") }
+//        return binding.root
+//    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val binding = LayoutBottomDialogBinding.inflate(layoutInflater)
         val defaultSentenceArray: Array<String> = if (Utils.getUserType(context)) {
             resources.getStringArray(R.array.companyOftenSentence)
         } else {
-            resources.getStringArray(R.array.normalOftenSentence)
+            resources.getStringArray(R.array.pictureUrls)
         }
         val list: MutableList<String> = ArrayList()
         list.addAll(defaultSentenceArray)
@@ -45,10 +68,20 @@ class BottomDialog : BottomSheetDialogFragment() {
         binding.rvSentenceList.adapter = BottomDialogAdapter(list)
         binding.tvAdd.setOnClickListener { ToastUtils.shortToast(context, "点击了添加") }
         binding.tvRevamp.setOnClickListener { ToastUtils.shortToast(context, "点击了修改") }
-        return binding.root
+
+
+        val root: View = binding.root
+        //这样创建dialog就这样传递style
+        val dialog = BottomSheetDialog(requireContext(), R.style.TransBottomSheetDialogStyle)
+        dialog.setContentView(root);
+        //设置宽度
+        val params: ViewGroup.LayoutParams = root.layoutParams
+        params.height = (0.75 * resources.displayMetrics.heightPixels).toInt()
+        root.layoutParams = params
+        return dialog
     }
 
-    override fun getTheme(): Int {
-        return R.style.TransBottomSheetDialogStyle
-    }
+//    override fun getTheme(): Int {
+//        return R.style.TransBottomSheetDialogStyle
+//    }
 }
